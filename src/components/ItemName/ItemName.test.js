416d7cb68d21306d99373950dc12cd26ff15c3c9
Hanna Varlamova <belinanya@gmail.com>
2018-06-08 18:21:38 +0300

    Snapshot Test

--- src/components/ItemName/ItemName.test.js
+++ src/components/ItemName/ItemName.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import ItemName from './ItemName';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ItemName />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('ok', () => {
+  const tree = renderer
+    .create(<ItemName name="Normal number: " value="120" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
